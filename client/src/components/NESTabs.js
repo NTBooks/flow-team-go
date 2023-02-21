0e5540b84d5721e8cbb87a0bce8223aa4f193d3b
Nicholas Tantillo <author@nicktantillo.com>
2023-02-20 20:01:04 -0500

    Added more tab icons

--- client/src/components/NESTabs.js
+++ client/src/components/NESTabs.js
@@ -40,7 +40,29 @@ image-rendering: pixelated;
 const RText = styled.div`
 display: inline-block;
 position:absolute;
-right:0;
+right:1rem;
+padding-top:0.7rem;
+font-size: 0.7rem;
+color:#fff1e8;
+text-shadow: 0.1rem 0.1rem 0.1rem #111111;
+`
+const RTextImage = styled.img`
+position: absolute;
+height: 2rem;
+top: 0;
+right: 3rem;
+image-rendering: pixelated;
+
+`
+
+const CollapseImage = styled.img`
+position: absolute;
+height: 2rem;
+top: 0;
+right: -0.5rem;
+z-index: 10001;
+image-rendering: pixelated;
+
 `
 
 const NESTabs = (props) => {
@@ -50,11 +72,27 @@ const NESTabs = (props) => {
     // set widths based on current tab
     const currTab = props.currTab ? props.currTab : 1;
 
+    // const collapsedA = <div style={{ fontSize: '0.7rem', position: 'absolute', top: '0.21rem', left: '0.5rem', zIndex: 10000 }}>
+    //     <a style={{ width: '1.5rem', backgroundColor: '#ffffff55' }} href="#" class="nes-badge"><span class="is-success">A</span>
+    //     </a>
+    // </div>;
+
+    // const collapsedB = currTab == 3 ? <div style={{ fontSize: '0.7rem', position: 'absolute', top: '0.21rem', left: '1rem', zIndex: 10000 }}>
+    //     <a style={{ width: '1.5rem', background: '#ffffff55' }} href="#" class="nes-badge"><span class="is-dark">B</span>
+    //     </a>
+    // </div> : <div style={{ fontSize: '0.7rem', position: 'absolute', top: '0.21rem', left: '20rem', zIndex: 10000 }}>Team B</div>;
+
+    const collapsedA = <CollapseImage src={require("../../public/AButton.png")}></CollapseImage>;
+    const collapsedB = <CollapseImage src={require("../../public/BButton.png")}></CollapseImage>;
+    const collapsedOpt = <CollapseImage src={require("../../public/GearButton.png")}></CollapseImage>;
+
+
     return <OverlapTabContainer>
-        <RText onClick={(e) => { props.setTab(3) }}>?</RText>
-        <OverlapTab onClick={(e) => { props.setTab(2) }} style={{ left: '6rem' }} width={currTab < 4 ? '30rem' : '2rem'} color="#209CEE">{currTab == 3 && 'Options'}<StubImage src={require("../../public/TabStubBU.png")}></StubImage></OverlapTab>
-        <OverlapTab onClick={(e) => { props.setTab(1) }} style={{ left: '3rem' }} width={currTab < 3 ? '24rem' : '2rem'} color="#92CC41">{currTab == 2 && 'Team B'}<StubImage src={require("../../public/TabStubGR.png")}></StubImage></OverlapTab>
-        <OverlapTab onClick={(e) => { props.setTab(0) }} width={currTab == 1 ? '18rem' : '2rem'} color="#E76E55">{currTab == 1 && 'Team A'}<StubImage src={require("../../public/TabStubR.png")}></StubImage></OverlapTab>
+        <RText onClick={(e) => { props.setTab(3) }}>PAGE<RTextImage src={require("../../public/SmControllerRight.png")}></RTextImage></RText>
+        <OverlapTab onClick={(e) => { props.setTab(2) }} style={{ left: '6rem' }} width={currTab < 4 ? '26rem' : '2rem'} color="#209CEE">{currTab == 3 ? 'Options' : collapsedOpt}<StubImage src={require("../../public/TabStubBU.png")}></StubImage></OverlapTab>
+        <OverlapTab onClick={(e) => { props.setTab(1) }} style={{ left: '3rem' }} width={currTab < 3 ? '20rem' : '2rem'} color="#92CC41">{currTab == 2 ? 'Team B' : collapsedB}<StubImage src={require("../../public/TabStubGR.png")}></StubImage></OverlapTab>
+        <OverlapTab onClick={(e) => { props.setTab(0) }} width={currTab == 1 ? '14rem' : '2rem'} color="#E76E55">{currTab == 1 ? 'Team A' : collapsedA
+        }<StubImage src={require("../../public/TabStubR.png")}></StubImage></OverlapTab>
     </OverlapTabContainer >
 
 }
